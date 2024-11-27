3bb872da7f95ac4b291388688e00dcb8dc08d62a
Ismael Berón <meikakuzen@gmail.com>
2024-11-27 23:56:22 +0100

    no validations dto

--- sleepr/apps/reservations/src/dto/create-reservation.dto.ts
+++ sleepr/apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,18 +1,18 @@
-import { IsDate, IsString } from "class-validator";
+
 
 export class CreateReservationDto {
-    @IsString()
+    
     startDate: Date;
     
-    @IsString()
+    
     endDate: Date;
     
-    @IsString()
+    
     userId: string;
     
-    @IsString()
+
     placeId: string;
     
-    @IsString()
+
     inVoiceId: string;
 }
